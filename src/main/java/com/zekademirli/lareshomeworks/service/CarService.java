cd51abb3d2f2e41c1811cd8bfdf7c0a9f441b297
zakadamirli <z3kkadamirli@gmail.com>
2024-11-25 13:58:11 +0400

    implement request && response classes | CarService.java

--- src/main/java/com/zekademirli/lareshomeworks/service/CarService.java
+++ src/main/java/com/zekademirli/lareshomeworks/service/CarService.java
@@ -3,6 +3,9 @@ package com.zekademirli.lareshomeworks.service;
 import com.zekademirli.lareshomeworks.dto.CarDTO;
 import com.zekademirli.lareshomeworks.entity.Car;
 import com.zekademirli.lareshomeworks.repository.CarRepository;
+import com.zekademirli.lareshomeworks.request.CreateCarRequest;
+import com.zekademirli.lareshomeworks.request.UpdateCarRequest;
+import com.zekademirli.lareshomeworks.response.CarResponse;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
@@ -17,20 +20,22 @@ public class CarService {
     private CarRepository carRepository;
     private final ModelMapper modelMapper;
 
-    public List<Car> getAllCars() {
-        return carRepository.findAll();
+    public List<CarResponse> getAllCars() {
+        List<Car> car = carRepository.findAll();
+        return car.stream().map(m -> modelMapper.map(m, CarResponse.class)).toList();
     }
 
-    public Car getCarById(Long id) {
-        return carRepository.findById(id).orElseThrow(() -> new RuntimeException("Car Not Found by  id : " + id));
+    public CarResponse getCarById(Long id) {
+        Car car = carRepository.findById(id).orElseThrow(() -> new RuntimeException("Car Not Found by  id : " + id));
+        return modelMapper.map(car, CarResponse.class);
     }
 
-    public void createCar(CarDTO car) {
+    public void createCar(CreateCarRequest car) {
         carRepository.save(modelMapper.map(car, Car.class));
         log.info("Create one car");
     }
 
-    public void updateCar(Long id, Car car) {
+    public void updateCar(Long id, UpdateCarRequest car) {
         Car updatedCar = carRepository.findById(id).orElseThrow(null);
 
         modelMapper.map(car, updatedCar);
@@ -41,13 +46,4 @@ public class CarService {
     public void deleteCar(Long id) {
         carRepository.deleteById(id);
     }
-
-    private CarDTO convertToDTO(Car car) {
-        return modelMapper.map(car, CarDTO.class);
-    }
-
-    private Car convertToEntity(CarDTO carDTO) {  //unit test problem
-        return modelMapper.map(carDTO, Car.class);
-    }
-
 }
