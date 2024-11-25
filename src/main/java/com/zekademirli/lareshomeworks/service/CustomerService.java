12bddd26d4f1c33faa0dd32d14f5750d61066b2b
zakadamirli <z3kkadamirli@gmail.com>
2024-11-25 14:16:09 +0400

    update CustomerService.java

--- src/main/java/com/zekademirli/lareshomeworks/service/CustomerService.java
+++ src/main/java/com/zekademirli/lareshomeworks/service/CustomerService.java
@@ -2,7 +2,11 @@ package com.zekademirli.lareshomeworks.service;
 
 import com.zekademirli.lareshomeworks.entity.Customer;
 import com.zekademirli.lareshomeworks.repository.CustomerRepository;
+import com.zekademirli.lareshomeworks.request.CreateCustomerRequest;
+import com.zekademirli.lareshomeworks.request.UpdateCustomerRequest;
+import com.zekademirli.lareshomeworks.response.CustomerResponse;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,22 +17,33 @@ import java.util.Optional;
 public class CustomerService {
 
     private CustomerRepository customerRepository;
+    private ModelMapper modelMapper;
 
-    public List<Customer> getAllCustomers() {
-        return customerRepository.findAll();
+    public List<CustomerResponse> getAllCustomers() {
+        List<Customer> customers = customerRepository.findAll();
+        return customers.stream().map(m -> modelMapper.map(m, CustomerResponse.class)).toList();
+
     }
 
-    public Optional<Customer> getCustomerById(Long id) {
-        return customerRepository.findById(id);
+    public Optional<CustomerResponse> getCustomerById(Long id) {
+        Customer customer = customerRepository.findById(id).orElse(null);
+        return customer == null ? Optional.empty() : Optional.of(modelMapper.map(customer, CustomerResponse.class));
     }
 
-    public Customer createCustomer(Customer customer) {
-        return customerRepository.save(customer);
+    public CustomerResponse createCustomer(CreateCustomerRequest customer) {
+        Customer customerEntity = modelMapper.map(customer, Customer.class);
+        Customer savedCustomer = customerRepository.save(customerEntity);
+        return modelMapper.map(savedCustomer, CustomerResponse.class);
     }
 
-    public Customer updateCustomer(Long id, Customer updatedCustomer) {
-        updatedCustomer.setId(id);
-        return customerRepository.save(updatedCustomer);
+    public CustomerResponse updateCustomer(Long id, UpdateCustomerRequest updatedCustomer) {
+        Customer customer = customerRepository.findById(id).orElse(null);
+        modelMapper.map(updatedCustomer, customer);
+        Customer savedCustomer = null;
+        if (customer != null) {
+            savedCustomer = customerRepository.save(customer);
+        }
+        return modelMapper.map(savedCustomer, CustomerResponse.class);
     }
 
     public void deleteCustomer(Long id) {
