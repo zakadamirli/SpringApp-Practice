6589ed49341ec768ad8070a20fd00799ac9706af
zakadamirli <z3kkadamirli@gmail.com>
2024-11-25 14:14:31 +0400

    create customerResponse class

---
+++ src/main/java/com/zekademirli/lareshomeworks/response/CustomerResponse.java
@@ -0,0 +1,12 @@
+package com.zekademirli.lareshomeworks.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CustomerResponse {
+    private String firstName;
+    private String lastName;
+    private Integer age;
+}
